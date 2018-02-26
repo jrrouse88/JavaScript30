37f851be0ef3dfceb84b713393075befab08fcbc
Jason Rouse <jrrouse88@gmail.com>
2018-02-26 15:12:16 -0800

    all elements selected

--- 11 - Custom Video Player/myScripts.js
+++ 11 - Custom Video Player/myScripts.js
@@ -1,5 +1,12 @@
 /* Get Our Elements */
-
+const theater = document.querySelector('.player');
+const screen = document.querySelector('.viewer');
+const runtime = document.querySelector('.progress');
+const methodButton = document.querySelector('.toggle');
+// select all ranges. will run a loop to assign functions
+const ranges = document.querySelectorAll('.player__slider');
+// select all skip buttons. will run a loop to assign functions
+const skipButtons = document.querySelectorAll('[data-skip]');
 /* Build out functions */
   //toggle play
 
