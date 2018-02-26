e64fed26e5a597c9a37f0e6af93fdf8db339aec0
Jason Rouse <jrrouse88@gmail.com>
2018-02-26 14:55:35 -0800

    new script file added with comments for what needs to be done

---
+++ 11 - Custom Video Player/myScripts.js
@@ -0,0 +1,16 @@
+/* Get Our Elements */
+
+/* Build out functions */
+  //toggle play
+
+  //update button text
+
+  //skip
+
+  //handle range update
+
+  //handle progress
+
+  //scrub (has an event)
+
+/* Hook up the event listeners */
